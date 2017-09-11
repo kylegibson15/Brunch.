4b8b7414d0016b8cb6ac58654bb39a84a2b55e32
kylegibson15 <kylegibson15@gmail.com>
2017-09-11 17:18:50 -0600

    seperated different functions into other html, js, and css files

---
+++ Home/app.js
@@ -0,0 +1,17 @@
+var favorites = document.getElementsByClassName('favorites')[0]
+var search = document.getElementsByClassName('search')[0]
+var featured = document.getElementsByClassName('featured')[0]
+var bottomless = document.getElementsByClassName('bottomless')[0]
+var rose = document.getElementsByClassName('rose')[0]
+
+
+  function favoritesPage() {
+
+    favorites.style.width = '100%'
+    favorites.style.height = '530px'
+
+    search.style.display = 'none'
+    featured.style.display = 'none'
+    bottomless.style.display = 'none'
+    rose.style.display = 'none'
+  }
