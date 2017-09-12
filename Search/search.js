db8e2e7bf457c5766fa1ae98c82eac4d1084f8d6
kylegibson15 <kylegibson15@gmail.com>
2017-09-12 15:25:41 -0600

    created an object to hold all menu items, acting as an api

--- Search/search.js
+++ Search/search.js
@@ -1,1 +1,31 @@
-
+var items = []
+var items2 = {}
+var items3 = []
+var value = 'chocolate'
+// for(var i = 0; i < menuItems.restaurants.length; i++){
+//   for(var j in )
+//   items.push(menuItems.restaurants[i])
+//   // console.log(items)
+// }
+for(var j in menuItems.restaurants[0]) {
+  items2.item = menuItems.restaurants[0][j].menu.items[0]
+  console.log(items2)
+}
+// for(var k = 0; k < items2.length; k++){
+//   for(var l in items2[k]){
+//     items3.push(items2[k][l].labels)
+//
+//   }
+  // console.log(items2)
+  // console.log(items3)
+// }
+// console.log(items3)
+for( var m = 0; m < items3.length; m++){
+  for(var o = 0; o < items3[m].length; o++){
+    if(value === items3[m][o]){
+      // console.log(`yay, we have ${value}, `)
+    } else{
+      // console.log(`fuck you`)
+    }
+  }
+}
--- menuItem/menuItemObject.js
+++ menuItem/menuItemObject.js
@@ -1,169 +1,86 @@
 var menuItems = {
-  'item':[
-    {
-      'pancakes':{
-        'name':'Pineapple Upside Down Pancakes',
-        'restaurant':'Snooze',
-        'description':'Buttermilk pancakes with caramelized pineapple chunks, housemade vanilla crème anglaise and cinnamon butter.',
-        'price':''
-      },
-      {
-        'name':'Sweet Potato Pancakes',
-        'restaurant':'Snooze',
-        'description':'Our signature sweet potato buttermilk pancakes topped with homemade caramel, candied pecans and ginger butter.',
-        'price':''
-      },
-      {
-        'name':'Blackberry SubLIME Pancakes',
-        'restaurant':'Snooze',
-        'description':'Buttermilk pancakes loaded with white chocolate chips then topped with Key Lime pie filling, graham cracker crumbs, plump blackberries, and mascarpone whip cream.',
-        'price':''
-      },
-      {
-        'name':'Blueberry Danish Pancakes',
-        'restaurant':'Snooze',
-        'description':'Buttermilk pancakes topped with blueberry coulis, sweet cream and almond streusel surrounding a center of lemony cream cheese filling',
-        'price':''
-      },
-      {
-        'name':'Pancake of the Day',
-        'restaurant':'Snooze',
-        'description':'Ask thy server for today’s flavor!',
-        'price':''
-      }
-    },
-    {
-      'french toast':{
+    "restaurants": [
         {
-          'name':'OMG! French Toast',
-          'restaurant':'Snooze',
-          'description':'Brioche stuffed with mascarpone then griddled and topped with vanilla crème, salted caramel, fresh strawberries and toasted coconut. O. M. G.',
-          'price':''
-        },
-        {
-          'name':'French Toast Neat',
-          'restaurant':'Snooze',
-          'description':'For the purist, French toasted brioche completed with whipped mascarpone cream, seasonal fruit and Slopeside pure Vermont maple syrup.',
-          'price':''
-        }
-      },
-    },
-    {
-      'eggs':{
-            'name':'The Snooze Classic',
-            'restaurant':'Snooze',
-            'description':'Three farm fresh cage free eggs cooked any style and your choice of ham, bacon, sausage, chorizo, pulled pork, chicken sausage or soyrizo. Includes hash browns and choice of toast.',
-            'price':''
-          },
-          {
-            'name':'3 Egg Omelet or Scramble',
-            'restaurant':'Snooze',
-            'description':'Our farm fresh eggs with a choice of three fillings served with hash browns and choice of toast or tortillas **Now available as a tofu scramble!**',
-            'price':''
-          },
-          {
-            'name':'Breakfast Pot Pie',
-            'restaurant':'Snooze',
-            'description':'Snooze’s homemade rosemary sausage gravy smothers a flaky puff pastry, topped with an egg your style (we’d say sunny side up!). You’ve reached breakfast bliss, complete with hash browns.',
-            'price':''
-          },
-          {
-            'name':'Snooze Spuds Deluxe',
-            'restaurant':'Snooze',
-            'description':'A heaping portion of our hash browns, covered with melted cheddar & jack cheese, scallions plus choice of two fillings – see below. Topped with two cage free eggs your style!',
-            'price':''
-          },
-          {
-            'name':'Corned Beef Hash',
-            'restaurant':'Snooze',
-            'description':'Snooze’s signature shredded hash mixed with locally made corned beef, caramelized poblanos and onions. Topped with two eggs your style & toast or tortillas.',
-            'price':''
-          },
-          {
-            'name':'Sweet Potato Hash',
-            'restaurant':'Snooze',
-            'description':'Sweet potato and fennel chicken sausage hash fired up with honey Cholula, carrots, poblanos, leeks, jack cheese and baby kale, topped with two cage free eggs your style.',
-            'price':''
-          },
-          {
-            'name':'uan’s Breakfast Tacos',
-            'restaurant':'Snooze',
-            'description':'Three fresh corn or flour tortillas topped with scrambled eggs, shredded hash browns, jack cheese, green chile hollandaise & pico de gallo.(inside tip: better yet with veggie/meat toppings)',
-            'price':''
-          },
-          {
-            'name':'Snooze Breakfast Burrito',
-            'restaurant':'Snooze',
-            'description':'Burrito Filled with scrambled eggs, hash browns, house black beans, cheddar & jack cheese, topped with pico de gallo and choice of green chili or ranchero.  (psst, love your veggies & meats)',
-            'price':''
-          },
-          {
-            'name':'Huevos Rancheros',
-            'restaurant':'Snooze',
-            'description':'Corn or flour tortillas layered with black beans, jack cheese, ranchero sauce, three eggs any style, and pico de gallo.(Imagine it topped with veggies and/or meats!)',
-            'price':''
-          }
-        },
-      },
-    {
-      'benedict':{
-              'name':'Ham Benedict III',
-              'restaurant':'Snooze',
-              'description':'The classic evolved with our signature English muffin topped with shaved hickory smoked ham, perfectly poached cage free eggs and smoked cheddar hollandaise.',
-              'price':''
+            "restaurant": {
+                "name": "Snooze",
+                "location": {
+                    "address": "2262 Larimer Street, Denver 80205",
+                    "locality": "LoDo",
+                    "city": "Denver",
+                    "city_id": 305,
+                    "latitude": "39.7556527778",
+                    "longitude": "-104.9889972222",
+                    "zipcode": "80205",
+                    "country_id": 216,
+                    "locality_verbose": "LoDo, Denver"
+                },
+                "menu":{
+                    "items":[
+                      {
+                        "Pineapple Upside Down Pancakes":{
+                            "description": 'Buttermilk pancakes with caramelized pineapple chunks, housemade vanilla crème anglaise and cinnamon butter.',
+                            "labels": ['pancake','pancakes','buttermilk','pineapple','vanilla','cinnamon','butter'],
+                            "price": 'n/a'
+                        },
+                        "Sweet Potato Pancakes":{
+                            "description": 'Our signature sweet potato buttermilk pancakes topped with homemade caramel, candied pecans and ginger butter.',
+                            "labels": ['pancake','pancakes','buttermilk','sweet potato','caramel','pecan','ginger','butter','candied'],
+                            "price": 'n/a'
+                        },
+                        "Blackberry SubLIME Pancakes":{
+                            "description": 'Buttermilk pancakes loaded with white chocolate chips then topped with Key Lime pie filling, graham cracker crumbs, plump blackberries, and mascarpone whip cream.',
+                            "labels": ['pancake','pancakes','buttermilk','chocolate','key lime','graham cracker','blackberries','mascarpone','whip cream'],
+                            "price": 'n/a'
+                        },
+                        "Blueberry Danish Pancakes":{
+                            "description": 'Buttermilk pancakes topped with blueberry coulis, sweet cream and almond streusel surrounding a center of lemony cream cheese filling',
+                            "labels":['pancake','pancakes','buttermilk','blueberry','sweet cream','almond','streusel','lemon','cream cheese'],
+                            "price": 'n/a'
+                        }
+                      }
+                    ]
+                }
             },
-            {
-              'name':'Summer Harvest Benny',
-              'restaurant':'Snooze',
-              'description':'Parmesan pesto quinoa cakes with fresh Caprese salad topped with two poached eggs, cream cheese hollandaise, balsamic reductions, and Parmesan crisps.',
-              'price':''
-            },
-            {
-              'name':'Chilaquiles Benedict',
-              'restaurant':'Snooze',
-              'description':'Flavorful barbacoa beef over a stack of ranchero sauced tortillas and melted cheese, with poached cage free eggs, roasted poblano hollandaise, pico de gallo and cotija cheese.',
-              'price':''
-            },
-            {
-              'name':'Benny Goodman',
-              'restaurant':'Snooze',
-              'description':'Lox style salmon and cream cheese served over toasted rye and topped with poached cage free eggs, cream cheese hollandaise blended with sun dried tomato and caper relish.',
-              'price':''
-            },
-            {
-              'name':'Bella! Bella! Benny',
-              'restaurant':'Snooze',
-              'description':'Thin slices of prosciutto, Taleggio cheese, and perfectly poached eggs on toasted ciabatta, topped with cream cheese hollandaise, balsamic glaze and arugula. Ciao Bella!',
-              'price':''
-            },
-            {
-              'name':'Chile Verde Benny',
-              'restaurant':'Snooze',
-              'description':'Slow cooked pulled pork over a stack of green chile sauced tortillas and melted cheese with poached cage free eggs and green chili hollandaise, pico de gallo and cotija cheese.',
-              'price':''
+            "restaurant2": {
+                "name": "Syrup",
+                "location": {
+                  "address": "999 18th Street, Suite 105, Denver 80202",
+                  "locality": "Central Business District",
+                  "city": "Denver",
+                  "city_id": 305,
+                  "latitude": "39.7488653000",
+                  "longitude": "-104.9915620000",
+                  "zipcode": "80202",
+                  "country_id": 216,
+                  "locality_verbose": "Central Business District, Denver"
+                },
+                "menu":{
+                    "items":[
+                      {
+                        "Beignets":{
+                            "description": 'lightly fried dough rolled in cinnamon sugar glaze and topped with powdered sugar',
+                            "labels": ['beignet','beignets','fried','cinnamon','sugar'],
+                            "price": '6.00'
+                        },
+                        "Pancake or Waffle Flight":{
+                            "description": 'choose either 4 sand dollar pancakes OR 3 waffle wedges',
+                            "labels": ['pancake','pancakes','buttermilk','waffle'],
+                            "price": '6.00'
+                        },
+                        "3 Little Pigs in a Blanket":{
+                            "description": '3 sand dollar pancakes stuffed with sweet maple sausage patties',
+                            "labels": ['pigs in a blanket','pancake','pancakes','sausage','maple'],
+                            "price": '6.00'
+                        },
+                        "Cracked Bacon":{
+                            "description": '5 pieces of our house crack bacon made with brown sugar and black pepper',
+                            "labels":['bacon','brown sugar','pepper'],
+                            "price": '6.00'
+                        }
+                      }
+                    ]
+                }
             }
-          },
-    },
-    {
-      'sandwiches':{
-          'name':'',
-          'restaurant':'',
-          'description':'',
-          'price':''
         },
-        {
-          'name':'',
-          'restaurant':'',
-          'description':'',
-          'price':''
-        },
-        {
-          'name':'',
-          'restaurant':'',
-          'description':'',
-          'price':''
-        }
-      }
-    },
-  ]
+    ]
 }
