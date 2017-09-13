79ec8e790618872b42a5e94e785b339fd8263fa0
kylegibson15 <kylegibson15@gmail.com>
2017-09-12 19:51:27 -0600

    search function can now access the input field

--- Search/search.js
+++ Search/search.js
@@ -1,29 +1,35 @@
-var value = 'bacon'
-var restaurantsArray = menuItems.restaurants
+var form = document.getElementById('food')
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  var id = event.target.elements.foodItem.value;
 
-function findMenuItems(value){
-  for(var i = 0; i < restaurantsArray.length; i++){
+  var restaurantsArray = menuItems.restaurants
 
-    var currentRestaurant = restaurantsArray[i]
-    var currentRestaurantName = currentRestaurant.restaurant.name
-    var currentRestaurantMenuItems = currentRestaurant.restaurant.menu.items
 
-    for(var j = 0; j < currentRestaurantMenuItems.length; j++){
-      for(var k in currentRestaurantMenuItems[j]){
+  function findMenuItems(id){
+    for(var i = 0; i < restaurantsArray.length; i++){
 
-        var currentLabels = currentRestaurantMenuItems[j][k].labels
-        var currentItem = currentRestaurantMenuItems[j][k].item
-        var currentDescription = currentRestaurantMenuItems[j][k].description
+      var currentRestaurant = restaurantsArray[i]
+      var currentRestaurantName = currentRestaurant.restaurant.name
+      var currentRestaurantMenuItems = currentRestaurant.restaurant.menu.items
 
-        for(var l = 0; l < currentLabels.length; l++){
-          if(value === currentLabels[l]){
-            console.log(`${currentItem}, ${currentDescription}`)
-          }else{
+      for(var j = 0; j < currentRestaurantMenuItems.length; j++){
+        for(var k in currentRestaurantMenuItems[j]){
 
+          var currentLabels = currentRestaurantMenuItems[j][k].labels
+          var currentItem = currentRestaurantMenuItems[j][k].item
+          var currentDescription = currentRestaurantMenuItems[j][k].description
+
+          for(var l = 0; l < currentLabels.length; l++){
+            if(id === currentLabels[l]){
+              console.log(`${currentItem}, ${currentDescription}`)
+            }else{
+
+            }
           }
         }
       }
     }
   }
-}
-findMenuItems(value)
+findMenuItems(id)
+})
