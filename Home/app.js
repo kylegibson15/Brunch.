2e91633e1f305bf160146c8ed64f78251c387671
kylegibson15 <kylegibson15@gmail.com>
2017-09-12 20:31:34 -0600

    added address and locale to restaurant search : dropdown menu on home

--- Home/app.js
+++ Home/app.js
@@ -15,3 +15,23 @@ var rose = document.getElementsByClassName('rose')[0]
     bottomless.style.display = 'none'
     rose.style.display = 'none'
   }
+  /* When the user clicks on the button,
+  toggle between hiding and showing the dropdown content */
+  function myFunction() {
+      document.getElementById("myDropdown").classList.toggle("show");
+  }
+
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
--- Search/search.js
+++ Search/search.js
@@ -4,7 +4,7 @@ form.addEventListener('submit', function(event) {
   var id = event.target.elements.foodItem.value;
 
   var restaurantsArray = menuItems.restaurants
-
+  var section = document.getElementById('foodItems')
 
   function findMenuItems(id){
     for(var i = 0; i < restaurantsArray.length; i++){
@@ -12,6 +12,8 @@ form.addEventListener('submit', function(event) {
       var currentRestaurant = restaurantsArray[i]
       var currentRestaurantName = currentRestaurant.restaurant.name
       var currentRestaurantMenuItems = currentRestaurant.restaurant.menu.items
+      var currentRestaurantAddress = currentRestaurant.restaurant.location.address
+      var currentRestaurantLocale = currentRestaurant.restaurant.location.locality
 
       for(var j = 0; j < currentRestaurantMenuItems.length; j++){
         for(var k in currentRestaurantMenuItems[j]){
@@ -22,6 +24,29 @@ form.addEventListener('submit', function(event) {
 
           for(var l = 0; l < currentLabels.length; l++){
             if(id === currentLabels[l]){
+
+              var div = document.createElement('div')
+              var h3 = document.createElement('h3')
+              var h5 = document.createElement('h5')
+              var p = document.createElement('p')
+              var address = document.createElement('p')
+              var locale = document.createElement('p')
+
+              h3.innerText = currentItem
+              h5.innerText = currentDescription
+              p.innerText = `you can find this at: ${currentRestaurantName}`
+              address.innerText = currentRestaurantAddress
+              locale.innerText = currentRestaurantLocale
+
+
+              section.append(div)
+              div.append(h3)
+              div.append(h5)
+              div.append(p)
+              div.append(address)
+              div.append(locale)
+
+
               console.log(`${currentItem}, ${currentDescription}`)
             }else{
 
