d0b0779f3338c998356ab22be5d33e50c1235318
kylegibson15 <kylegibson15@gmail.com>
2017-09-14 11:15:09 -0600

    fixed issue with error on rose api call and added divider on home pg

--- public/Rose/rose.js
+++ public/Rose/rose.js
@@ -9,17 +9,17 @@ fetch('https://galvanize-cors.herokuapp.com/http://api.snooth.com/wines/?akey=9m
     var div = document.createElement('div')
       div.classList.add('roseOfTheMonth')
     var wineName = document.createElement('h5')
-      wineName.innerText = roseData.wines[1].name
+      wineName.innerText = roseData.wines[5].name
     var winery = document.createElement('h6')
-      winery.innerText = roseData.wines[1].winery
+      winery.innerText = roseData.wines[5].winery
     var img = document.createElement('img')
-      img.setAttribute('src', roseData.wines[1].image)
+      img.setAttribute('src', roseData.wines[5].image)
     var price = document.createElement('p')
-      price.innerText = `$${roseData.wines[1].price}`
+      price.innerText = `$${roseData.wines[5].price}`
     var region = document.createElement('p')
-      region.innerText = roseData.wines[1].region
+      region.innerText = roseData.wines[5].region
     var year = document.createElement('p')
-      year.innerText = roseData.wines[1].vintage
+      year.innerText = roseData.wines[5].vintage
 
     roseBody.append(div)
     div.append(wineName)
