5f7d8397873271dbe6b77387e05e56551439dd6d
kylegibson15 <kylegibson15@gmail.com>
2017-09-14 13:09:46 -0600

    completely functioning as a website

--- public/Rose/rose.js
+++ public/Rose/rose.js
@@ -1,5 +1,5 @@
 var roseBody = document.getElementById('rose')
-
+var num = 11
 fetch('https://galvanize-cors.herokuapp.com/http://api.snooth.com/wines/?akey=9moelcmue00e34o3p6apewqxmwyfuyk7dg15mlg0e8fabsgz&ip=66.28.234.115&q=rose&xp=30')
 .then((response) => {
   return response.json()
@@ -8,25 +8,37 @@ fetch('https://galvanize-cors.herokuapp.com/http://api.snooth.com/wines/?akey=9m
 
     var div = document.createElement('div')
       div.classList.add('roseOfTheMonth')
+    var imgDiv = document.createElement('div')
+      imgDiv.classList.add('imgDiv')
+    var spaceDiv = document.createElement('div')
+      spaceDiv.classList.add('spaceDiv')
+    var nameDescriptionDiv = document.createElement('div')
+      nameDescriptionDiv.classList.add('description')
+    var wineNameDiv = document.createElement('div')
+      wineNameDiv.classList.add('wineNameDiv')
     var wineName = document.createElement('h5')
-      wineName.innerText = roseData.wines[5].name
+      wineName.innerText = roseData.wines[`${num}`].name
     var winery = document.createElement('h6')
-      winery.innerText = roseData.wines[5].winery
+      winery.innerText = `Winery: ${roseData.wines[`${num}`].winery}`
     var img = document.createElement('img')
-      img.setAttribute('src', roseData.wines[5].image)
+      img.setAttribute('src', roseData.wines[`${num}`].image)
+      img.classList.add('wineImage')
     var price = document.createElement('p')
-      price.innerText = `$${roseData.wines[5].price}`
+      price.innerText = `$${roseData.wines[`${num}`].price}`
     var region = document.createElement('p')
-      region.innerText = roseData.wines[5].region
+      region.innerText = `Region: ${roseData.wines[`${num}`].region}`
     var year = document.createElement('p')
-      year.innerText = roseData.wines[5].vintage
+      year.innerText = `Year: ${roseData.wines[`${num}`].vintage}`
 
-    roseBody.append(div)
-    div.append(wineName)
-    div.append(img)
-    div.append(winery)
-    div.append(price)
-    div.append(region)
-    div.append(year)
+    roseBody.append(nameDescriptionDiv)
+    wineNameDiv.append(wineName)
+      nameDescriptionDiv.append(wineNameDiv)
+      nameDescriptionDiv.append(spaceDiv)
+        spaceDiv.append(winery)
+        spaceDiv.append(region)
+        spaceDiv.append(year)
+        spaceDiv.append(price)
+    roseBody.append(imgDiv)
+      imgDiv.append(img)
   })
 })
--- public/app.js
+++ public/app.js
@@ -1,30 +1,30 @@
-var featured = ['snooze','tamayo','the nickel','la loma','beatrice & woodsley']
-
-for(var i = 0; i < featured.length; i++){
-  fetch(`https://developers.zomato.com/api/v2.1/search?q=${featured[i]}&apikey=b23400682136545e76a3ebb355532993`)
-  .then((response) => {
-    return response.json()
-    .then((restaurantData) => {
-      console.log(restaurantData.restaurants[0].restaurant)
-      var list = document.getElementById('featured-restaurants-preview')
-      var div = document.createElement('div')
-        div.classList.add('restaurantInfo')
-      var name = document.createElement('h6')
-        name.innerText = restaurantData.restaurants[0].restaurant.name
-      var rating = document.createElement('p')
-        rating.innerText = restaurantData.restaurants[0].restaurant.user_rating.aggregate_rating
-      var image = document.createElement('img')
-        image.setAttribute('src',restaurantData.restaurants[0].restaurant.featured_image)
-        image.classList.add('restaurantImage')
-
-
-      list.append(div)
-      div.append(name)
-      div.append(image)
-      div.append(rating)
-    })
-  })
-}
+// var featured = ['snooze','tamayo','the nickel','la loma','beatrice & woodsley']
+//
+// for(var i = 0; i < featured.length; i++){
+//   fetch(`https://developers.zomato.com/api/v2.1/search?q=${featured[i]}&apikey=b23400682136545e76a3ebb355532993`)
+//   .then((response) => {
+//     return response.json()
+//     .then((restaurantData) => {
+//       console.log(restaurantData.restaurants[0].restaurant)
+//       var list = document.getElementById('featured-restaurants-preview')
+//       var div = document.createElement('div')
+//         div.classList.add('restaurantInfo')
+//       var name = document.createElement('h6')
+//         name.innerText = restaurantData.restaurants[0].restaurant.name
+//       var rating = document.createElement('p')
+//         rating.innerText = restaurantData.restaurants[0].restaurant.user_rating.aggregate_rating
+//       var image = document.createElement('img')
+//         image.setAttribute('src',restaurantData.restaurants[0].restaurant.featured_image)
+//         image.classList.add('restaurantImage')
+//
+//
+//       list.append(div)
+//       div.append(name)
+//       div.append(image)
+//       div.append(rating)
+//     })
+//   })
+// }
 
 var gifSection = document.getElementsByClassName('gif-container')[0];
 
