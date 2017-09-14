6efc689545666043a49514a179c2a478eb93148c
kylegibson15 <kylegibson15@gmail.com>
2017-09-14 10:42:13 -0600

    added cors to wine api

--- public/Rose/rose.js
+++ public/Rose/rose.js
@@ -1,6 +1,6 @@
 var roseBody = document.getElementById('rose')
 
-fetch('http://api.snooth.com/wines/?akey=9moelcmue00e34o3p6apewqxmwyfuyk7dg15mlg0e8fabsgz&ip=66.28.234.115&q=rose&xp=30')
+fetch('https://galvanize-cors.herokuapp.com/http://api.snooth.com/wines/?akey=9moelcmue00e34o3p6apewqxmwyfuyk7dg15mlg0e8fabsgz&ip=66.28.234.115&q=rose&xp=30')
 .then((response) => {
   return response.json()
   .then((roseData) => {
@@ -9,17 +9,17 @@ fetch('http://api.snooth.com/wines/?akey=9moelcmue00e34o3p6apewqxmwyfuyk7dg15mlg
     var div = document.createElement('div')
       div.classList.add('roseOfTheMonth')
     var wineName = document.createElement('h5')
-      wineName.innerText = roseData.wines[5].name
+      wineName.innerText = roseData.wines[1].name
     var winery = document.createElement('h6')
-      winery.innerText = roseData.wines[5].winery
+      winery.innerText = roseData.wines[1].winery
     var img = document.createElement('img')
-      img.setAttribute('src', roseData.wines[5].image)
+      img.setAttribute('src', roseData.wines[1].image)
     var price = document.createElement('p')
-      price.innerText = `$${roseData.wines[5].price}`
+      price.innerText = `$${roseData.wines[1].price}`
     var region = document.createElement('p')
-      region.innerText = roseData.wines[5].region
+      region.innerText = roseData.wines[1].region
     var year = document.createElement('p')
-      year.innerText = roseData.wines[5].vintage
+      year.innerText = roseData.wines[1].vintage
 
     roseBody.append(div)
     div.append(wineName)
